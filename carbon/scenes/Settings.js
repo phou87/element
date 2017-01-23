11330d24de21872e154dff02d0af360c2e3bc551
Paul Hou <lostinthought87@gmail.com>
2017-01-22 21:47:23 -0500

    Show push notification setting

--- carbon/scenes/Settings.js
+++ carbon/scenes/Settings.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import Parse from 'parse/react-native';
-import {Switch} from 'react-native';
+import {AppState, PushNotificationIOS, Switch} from 'react-native';
 
 import {Button, Icon, List, ListItem, Text} from 'native-base';
 
 class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      permissions: {},
+    };
+
+    this.checkPermissions = this.checkPermissions.bind(this);
+  }
+
+  componentWillMount() {
+    AppState.addEventListener('change', this.checkPermissions);
+		this.checkPermissions();
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.checkPermissions);
+  }
+
+  checkPermissions() {
+    PushNotificationIOS.checkPermissions(permissions => this.setState({permissions}));
+  }
+
   render() {
     return (
       <List>
         <ListItem iconLeft>
           <Icon name="ios-notifications" style={{ color: '#5bc0de' }} />
-          <Text>Push Notifications</Text>
-          <Switch
-            onValueChange={this.setNotifyFriendsSwitch}
-            value={true}
-          />
+          <Text>Push Notifications are {this.state.permissions.alert ? 'On' : 'Off'}</Text>
         </ListItem>
         <ListItem iconLeft>
           <Button block info onPress={this.props.onLogout}>Logout</Button>
