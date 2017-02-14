075f4e5c76100a78957d9e5bac058252137dd650
Swapnil Parikh <swap@Swapnils-MacBook-Pro.local>
2017-02-14 13:24:04 -0500

    swapupdate

--- hydrogen/index.ios.js
+++ hydrogen/index.ios.js
@@ -8,28 +8,30 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
+
+import {Button, CheckBox, Icon, Input, InputGroup, List, ListItem, Text, Textarea} from 'native-base';
+
 
 export default class hydrogen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+            <View style={styles.container}>
+            <List>
+            <ListItem>
+            <InputGroup>
+            <Icon name='ios-search' style={{color: '#5cb85c'}}/>
+            <Input placeholder="Enter a Stock Symbol" />
+            </InputGroup>
+            </ListItem>
+            </List>
+            </View>
+            
+            );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
